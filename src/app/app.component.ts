b4993c0183061f537c044ca2d4901db350da5584
Khachatur Gevorgyan <khachatur.gevorgyan.1993@gmail.com>
2021-08-02 22:36:20 +0400

    Adding validators to form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'epam-angular-forms-app';
+  form!: FormGroup;
+  /*
+  model = {
+    'password': this.form.controls?.password,
+    'confirmPassword': this.form.controls?.password_confirm,
+  };
+*/
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      surname: new FormControl(null, [Validators.required]),
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z.-]+\\.[a-z]{2,4}$'),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      /*     password_confirm: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),   */
+    });
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log('Form: ', this.form);
+      //     const formData = { ...this.form.value };
+      //     console.log('Form Data:', formData);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+// import { ValidateEqualModule } from 'ng-validate-equal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  //  ValidateEqualModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
