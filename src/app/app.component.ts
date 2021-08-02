09fa17684280019eb606ff42c689e5ecab43541d
Khachatur Gevorgyan <khachatur.gevorgyan.1993@gmail.com>
2021-08-02 23:11:59 +0400

    deploy error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   title = 'epam-angular-forms-app';
